ded6c9e9dc6244508dd4b279b10fe2b6902e5858
Inês A. Sousa <isousa@student.42lisboa.com>
2021-08-01 20:13:28 +0100

    threads are working at the same time

--- philo.c
+++ philo.c
@@ -1,24 +1,38 @@
 #include "philo.h"
 
-void    *philo1(void    *arg)
+void    *philo1(void *arg)
 {
-    t_set *ptr;
+    int *ptr;
+    ptr = (int *)arg;
 
-    ptr = (t_set *)arg;
-     while (1)
+    printf("AQUI\n");    
+    if ((*ptr) == 0)
     {
-        sleep(1);
-        printf("I'm Philo 1  -- %d \n", ptr->nb_philo);
+        printf("this is philo 1\n");
+        sleep(5);
+        printf("this is philo 1 for the 2n time\n");
 
     }
+    if ((*ptr) == 1)
+    {
+        printf("this is philo 2\n");
+        sleep(5);
+        printf("this is philo 2 for the 2n time\n");
+    }
+    if ((*ptr) == 2)
+    {
+        printf("this is philo 3\n");
+        sleep(5);
+        printf("this is philo 3 for the 2n time\n");
+    }
+
+    return NULL;
 }
 
 int main(int argc, char **argv)
 {
     t_set set;
-    pthread_t id;
-    pthread_t id2;
-
+    pthread_t *id = NULL;
 
     if (argc < 5 || argc > 6 || (check_arg(argv) == 1))
     {
@@ -28,25 +42,27 @@ int main(int argc, char **argv)
     init_set(&set);
     set_set(argv, &set);
 
+    // allocate memory for id of philos
+    id = malloc(sizeof(pthread_t) * (set.nb_philo));
+    
     int i = 0;
 
-    while (i < 2)
+    while (i < 3)
     {
-        pthread_create(&id, NULL, philo1, &set);
-        pthread_join(id, NULL);
+        printf("CHEGOU\n");    
+
+        pthread_create(&(id[i]), NULL, philo1, &i);
+        sleep(1);
+        
         i++;
-
-
     }
 
-    pthread_create(&id2, NULL, philo1, &set);   
-
-    pthread_join(id, NULL);
-    pthread_join(id2, NULL);
-
-
-    
-
+    i = 0;
+    while (i < 3)
+    {
+        pthread_join(id[i], NULL);
+        i++;
+    }
 
     return (0);
 }
